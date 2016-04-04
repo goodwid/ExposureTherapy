9bd776ee7078b737f053f35ddfffaf47605ff1b4
Taylor Zelaya <taylormzelaya@gmail.com>
2016-04-04 13:06:20 -0700

    amended list of what localStorage should store

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 /*
 localStorage should store:
 1. whether user has submitted evaluation (get started) form
-2. results of eval form (recommended starting point)
-3. whether user has begun exercises
-4. image viewed when user last left page
-5. previous visit dates and respective "level reached" numbers (for chart)
-6.
+2. user's name and panic image choice
+3. results of eval form (recommended starting point)
+4. whether user has begun exercises
+5. image viewed when user last left page
+6. previous visit dates and respective "level reached" numbers (for chart)
+7.
 
 */
