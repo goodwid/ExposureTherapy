8ac46e9a126faf73ef6f936891973811a0146eeb
Taylor Zelaya <taylormzelaya@gmail.com>
2016-04-04 13:01:51 -0700

    started list of what localStorage should track

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+/*
+localStorage should store:
+1. whether user has submitted evaluation (get started) form
+2. results of eval form (recommended starting point)
+3. whether user has begun exercises
+4. image viewed when user last left page
+5. previous visit dates and respective "level reached" numbers (for chart)
+6.
+
+*/
