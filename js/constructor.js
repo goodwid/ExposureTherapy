dd2d50e89d79819c65ca9d4da3666025c1a2cd57
Taylor Zelaya <taylormzelaya@gmail.com>
2016-04-05 13:15:05 -0700

    Built all image objects

--- js/constructor.js
+++ js/constructor.js
@@ -1,6 +1,49 @@
+//Image object constructor
 function Image(fileName) {
     this.path = '../images/' + fileName;
 }
 
+//Build all image objects
 var image1a = new Image('1a.png');
-console.log(image1a);
+var image1b = new Image('1b.png');
+var image1c = new Image('1c.png');
+
+var image2a = new Image('2a.png');
+var image2b = new Image('2b.png');
+var image2c = new Image('2c.png');
+
+var image3a = new Image('3a.png');
+var image3b = new Image('3b.png');
+var image3c = new Image('3c.png');
+
+var image4a = new Image('4a.jpg');
+var image4b = new Image('4b.jpg');
+var image4c = new Image('4c.jpg');
+
+var image5a = new Image('5a.gif');
+var image5b = new Image('5b.gif');
+var image5c = new Image('5c.gif');
+
+var image6a = new Image('6a.jpg');
+var image6b = new Image('6b.jpg');
+var image6c = new Image('6c.jpg');
+
+var image7a = new Image('7a.jpg');
+var image7b = new Image('7b.jpg');
+var image7c = new Image('7c.jpg');
+
+var image8a = new Image('8a.gif');
+var image8b = new Image('8b.gif');
+var image8c = new Image('8c.gif');
+
+var image9a = new Image('9a.jpg');
+var image9b = new Image('9b.png');
+var image9c = new Image('9c.jpg');
+
+var image10a = new Image('10a.png');
+var image10b = new Image('10b.png');
+var image10c = new Image('10c.png');
+
+var image11a = new Image('11a.png');
+var image11b = new Image('11b.png');
+var image11c = new Image('11c.png');
