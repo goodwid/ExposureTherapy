34399c65786dd5e2ca88359c05f983b04239ce1e
Taylor Zelaya <taylormzelaya@gmail.com>
2016-04-05 21:20:24 -0700

    function capitalizeName built, for-loop written to populate spans with class userName

--- js/personalize.js
+++ js/personalize.js
@@ -1,6 +1,13 @@
 var spanEls = document.getElementsByClassName('userName');
+var userInfoDestringified = JSON.parse(localStorage.userInfo);
+var userFirstName = capitalizeName(userInfoDestringified.userName);
 
+//capitalize userName in case user didn't
+function capitalizeName(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+}
 
+//populate all spans with class 'userName' with (capitalized) user name
 for (var bb=0; bb<spanEls.length; bb++) {
-    spanEls[bb].textContent = ;
+    spanEls[bb].textContent = userFirstName;
 }
