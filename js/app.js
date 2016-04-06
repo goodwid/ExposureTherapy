bf10b1ae595bad0aa9de39fe104c72288a2b3ad7
goodwid <goodwid@gmail.com>
2016-04-06 10:33:24 -0700

    T/F Form logic completed!

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ localStorage should store:
 var userInfo = {
   evalComplete: false,
   userName: '',
-  recommendedStartLevel: 1,
+  recommendedStartLevel: 10,
   exercisesBegun: false,
   lastLevelIndex: 1,
   lastImageIndex: 0,
@@ -25,23 +25,29 @@ var form = gebi('questionForm');
 var formButton = gebi('submitForm');
 if (formButton) {
     formButton.addEventListener('click', function () {
-        var answers=[];
         var userName = form.elements.inputName.value;
         var panicImageIndex = parseInt(form.elements.panicimage.value);
-        if (!userName) {
-            alert('Username is required!  Please fill out your first name.');
+        var phobia = form.elements.phobia.value;
+        if (phobia === 'other') {
+            alert('We regret that currently, Conquer It! only addresses arachnophobia.');
+            gebi('arach').checked = true;
         } else {
-            answers.push(userName)
-            answers.push(form.elements.phobia.value);
-            answers.push(panicImageIndex);
-            answers.push(form.elements.q1.value);
-            answers.push(form.elements.q2.value);
-            answers.push(form.elements.q3.value);
-            answers.push(form.elements.q4.value);
-            answers.push(form.elements.q5.value);
-            answers.push(form.elements.q6.value);
+            if (!userName) {
+                alert('Username is required!  Please fill out your first name.');
+            } else {
+                userInfo.userName = userName;
+                userInfo.panicImageIndex = panicImageIndex;
+
+                if (form.elements.q5.value === 'true') { userInfo.recommendedStartLevel = 10;}
+                if (form.elements.q4.value === 'true') { userInfo.recommendedStartLevel = 8;}
+                if (form.elements.q3.value === 'true') { userInfo.recommendedStartLevel = 5;}
+                if (form.elements.q2.value === 'true') { userInfo.recommendedStartLevel = 2;}
+                if (form.elements.q1.value === 'true') { userInfo.recommendedStartLevel = 1;}
+
+                console.log(userInfo.recommendedStartLevel);
+                updateUserInfo();
+            }
         }
-        var visits = userInfo.previousVisitLevels.length;
     })
 }
 
