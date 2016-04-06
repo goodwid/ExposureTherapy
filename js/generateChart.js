593dd14367c1037cde188c0fe41c9a1a3296a439
Taylor Zelaya <taylormzelaya@gmail.com>
2016-04-05 20:17:42 -0700

    Begin function generateChartData

---
+++ js/generateChart.js
@@ -0,0 +1,15 @@
+/*
+Assuming userInfo object's previousVisitData will be replaced by:
+previousVisitLevels
+previousVisitEvalResults
+*/
+
+//Examples to work from:
+//visitLevels: [2, 4, 7, 8];
+//visitEvalResults: [6, 5, 5, 4];
+
+function generateChartData(visitInfo) {     //where parameter visitInfo is either visitLevels or visitEvalResults
+    var visitData = {};
+    visitData.visits = [];
+    for (aa=0; aa<userInfo[chart])
+}
