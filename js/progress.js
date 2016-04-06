d5d51d0494a8abe1d12fad0ee21262046fa99927
Taylor Zelaya <taylormzelaya@gmail.com>
2016-04-06 12:06:22 -0700

    JS logic works, class added to inputs for numerical questions 1-2

--- js/progress.js
+++ js/progress.js
@@ -1,20 +1,24 @@
 var progressForm = gebi('progressForm');
 var progressFormButton = gebi('submitProgressForm');
 if (progressFormButton) {
-    progressFormButton.addEventListener('click', )
+    progressFormButton.addEventListener('click', processProgressForm);
 }
 
 function processProgressForm() {
     var anxietyIndex = 0;
     for (var cc=1; cc<=10; cc++) {
         var qName = 'q' + cc;
-        var selector = 'input[name = ' + qName + ']:checked';
-        var rating = document.querySelector(selector.)
-    }
-    var phobia = form.elements.phobia.value;
-            userInfo.panicImageIndex = panicImageIndex;
-
-            updateUserInfo();
+        var inputEls = document.getElementsByClassName(qName);
+        for (var dd=0; dd<inputEls.length; dd++) {
+            if (inputEls[dd].checked) {
+                console.log(inputEls[dd].value);
+                anxietyIndex += parseInt(inputEls[dd].value);
+            }
         }
     }
+
+    console.log(anxietyIndex);
+    userInfo.previousVisitAnxiety.push(anxietyIndex);
+
+    updateUserInfo();
 }
