6244030f0313921330b0c08e24f33972e5955df8
Taylor Zelaya <taylormzelaya@gmail.com>
2016-04-06 21:00:48 -0700

    Cleared TZ comments that aren't needed anymore

--- js/app.js
+++ js/app.js
@@ -1,14 +1,5 @@
-/*
-localStorage should store:
-1. whether user has submitted evaluation (get started) form
-2. user's name and panic image choice
-3. results of eval form (recommended starting point)
-4. whether user has begun exercises
-5. image viewed when user last left page
-6. previous visit dates and respective "level reached" numbers (for chart)
-*/
 var userInfo = {};
-//OBJECT THAT WILL BE STORED IN LOCALSTORAGE AND WILL TRACK USER DETAILS/PROGRESS
+
 function initUserInfo() {
     if (localStorage.userInfo) {
         userInfo = JSON.parse(localStorage.userInfo);
