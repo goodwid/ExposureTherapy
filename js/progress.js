1c735fa0ab6f93fcd203956d7a1f4b6b4756f626
Taylor Zelaya <taylormzelaya@gmail.com>
2016-04-06 20:37:49 -0700

    Declared showQuestionnaire() and showCharts()

--- js/progress.js
+++ js/progress.js
@@ -1,8 +1,15 @@
 var chartSection = gebi('chartSection');
 var formPopup = gebi('formPopup');
 
-window.onload = showQuestionnaire();
-
 function showQuestionnaire() {
-
+    chartSection.style.display = 'none';
+    formPopup.style.display = 'block';
 }
+
+function showCharts() {
+    formPopup.style.display = 'none';
+    chartSection.style.display = 'flex';
+}
+
+
+window.onload = showQuestionnaire();
